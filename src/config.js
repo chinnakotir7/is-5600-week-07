b7014c51d470a23382b43a249980c1fbafccecb1
chinnakotir7 <chinnakotir@gmail.com>
2025-03-05 05:17:35 +0000

    week 7 lab

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://musical-space-telegram-5gr9x76g65952v64j-3080.app.github.dev";
